b9deaa81e5d006d9715a16c18d5c5477eb9dc517
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2022-05-19 00:49:48 +0700

    Initial commit

---
+++ app/src/main/java/com/main/consultant/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.main.consultant
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView.OnEditorActionListener
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.api.gax.core.FixedCredentialsProvider
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.dialogflow.v2.*
+import com.main.consultant.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    //binding
+    private lateinit var binding: ActivityMainBinding
+
+    private var messageList: ArrayList<Message> = ArrayList()
+
+    private val TAG = "MainActivity"
+    private lateinit var chatAdapter: ChatAdapter
+    //dialogFlow
+    private var sessionsClient: SessionsClient? = null
+    private var sessionName: SessionName? = null
+    private val uuid = UUID.randomUUID().toString()
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //setting adapter to recyclerview
+        chatAdapter = ChatAdapter(this, messageList)
+        binding.chatView.adapter = chatAdapter
+
+        //onclick listener to update the list and call dialogflow
+        binding.btnSend.setOnClickListener {
+            val message: String = binding.editMessage.text.toString()
+            if (message.isNotEmpty()) {
+                addMessageToList(message, false)
+                sendMessageToBot(message)
+            } else {
+                Toast.makeText(this@MainActivity, "Please enter text!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.editMessage.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (event != null && event.keyCode == KeyEvent.KEYCODE_ENTER || actionId == EditorInfo.IME_ACTION_DONE) {
+                binding.btnSend.performClick()
+            }
+            false
+        })
+
+        setUpBot()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun addMessageToList(message: String, isReceived: Boolean) {
+        //handles UI changes
+        messageList.add(Message(message, isReceived))
+        binding.editMessage.setText("")
+        chatAdapter.notifyDataSetChanged()
+        binding.chatView.layoutManager?.scrollToPosition(messageList.size - 1)
+    }
+
+    private fun setUpBot() {
+        try {
+            val stream = this.resources.openRawResource(R.raw.credential)
+            val credentials: GoogleCredentials = GoogleCredentials.fromStream(stream)
+                .createScoped("https://www.googleapis.com/auth/cloud-platform")
+            val projectId: String = (credentials as ServiceAccountCredentials).projectId
+            val settingsBuilder: SessionsSettings.Builder = SessionsSettings.newBuilder()
+            val sessionsSettings: SessionsSettings = settingsBuilder.setCredentialsProvider(
+                FixedCredentialsProvider.create(credentials)
+            ).build()
+            sessionsClient = SessionsClient.create(sessionsSettings)
+            sessionName = SessionName.of(projectId, uuid)
+            Log.d(TAG, "projectId : $projectId")
+        } catch (e: Exception) {
+            Log.d(TAG, "setUpBot: " + e.message)
+        }
+    }
+
+    private fun sendMessageToBot(message: String) {
+        val input = QueryInput.newBuilder()
+            .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build()
+        GlobalScope.launch {
+            sendMessageInBg(input)
+        }
+    }
+
+    private suspend fun sendMessageInBg(
+        queryInput: QueryInput
+    ) {
+        withContext(Default) {
+            try {
+                val detectIntentRequest = DetectIntentRequest.newBuilder()
+                    .setSession(sessionName.toString())
+                    .setQueryInput(queryInput)
+                    .build()
+                val result = sessionsClient?.detectIntent(detectIntentRequest)
+                if (result != null) {
+                    runOnUiThread {
+                        updateUI(result)
+                    }
+                }
+            } catch (e: java.lang.Exception) {
+                Log.d(TAG, "doInBackground: " + e.message)
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun updateUI(response: DetectIntentResponse) {
+        val botReply: String = response.queryResult.fulfillmentText
+        if (botReply.isNotEmpty()) {
+            addMessageToList(botReply, true)
+        } else {
+            Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/main/consultant/Message.kt
@@ -0,0 +1,6 @@
+package com.main.consultant
+
+data class Message(
+    var message: String,
+    var isReceived: Boolean
+)
